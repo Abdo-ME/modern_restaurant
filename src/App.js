f03f304c7fc023a087f081ea5826797e2cfa10dd
Abdo-ME <abdelhakmehda@gmail.com>
2022-05-25 23:53:36 +0100

    add Navbar and header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="app">
-      <h1>Hello, world</h1>
-    </div>
-  );
-}
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+console.log('01:08, last One')
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
